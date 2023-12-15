90e977548a2dd55214269870f81222508e5883b7
developer.mezbah <developer.mezbah@gmail.com>
2023-12-15 20:43:05 +0600

    preview

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export async function generateMetadata() {
     title: "News project in NEXT.JS",
     description: "I am a highly motivated and detail-oriented software engineer with a passion for creating efficient, scalable, and maintainable software solutions. With 2 years of experience in the field, I have a proven track record of delivering high-quality code within challenging deadlines. My expertise lies in MERN Stack and NEXT.JS, and I am well-versed in the entire software development life cycle. I excel in problem-solving and thrive in dynamic and collaborative team environments.",
     openGraph: {
-      images: "/preview.png",
+      images: "https://raw.githubusercontent.com/developer-mezbah/next-news/main/public/preview.png?token=GHSAT0AAAAAACHT5MOOGBJ6AJ6XPSPA5QV4ZL4MYGQ",
     },
   }
 }
