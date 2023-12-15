8d758154731eeaf024baba6ea801268a3a6feb60
developer.mezbah <developer.mezbah@gmail.com>
2023-12-15 19:48:20 +0600

    loadding

--- src/app/create-post/loading.jsx
+++ src/app/create-post/loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
     return (
-      <div className="fixed bg-transparent inset-0 flex items-center justify-center">
+      <div className="fixed bg-transparent inset-0 flex items-center justify-center bg-white">
         <div className="loader w-16 h-16 border-t-4 border-themeColor border-solid rounded-full animate-spin"></div>
       </div>
     );
